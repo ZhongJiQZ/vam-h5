e2afa212c048723d3f9fda96e6ee9af3b84e1b30
0 <0>
2026-03-24 22:56:39 +0800

    侧边栏

--- vam-h5/src/views/slidebar/add-address.vue
+++ vam-h5/src/views/slidebar/add-address.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="bg">
-    <HeaderBar :currentName="_t18('bind_address')" :border_bottom="true"></HeaderBar>
-    <div class="form">
-      <div class="form-item">
-        <div class="label">
-          <!-- 请选择链 -->
-          {{ _t18('please_select_a_chain') }}
-        </div>
-        <div class="input" @click="openCoinSheet">
-          <img :src="iconSrcImg" class="coin" />
-          <div class="tit">{{ form.type }}</div>
-          <div class="tip">
-            <van-icon name="arrow-down" />
-          </div>
+  <div class="page-add-address">
+    <DarkHeaderBar :title="_t18('bind_address')" :border_bottom="true" />
+
+    <div class="page-body">
+      <div class="card">
+        <div class="field-label">{{ _t18('please_select_a_chain') }}</div>
+        <div class="chain-select" role="button" tabindex="0" @click="openCoinSheet">
+          <img :src="iconSrcImg" class="coin" alt="" />
+          <span class="chain-name">{{ form.type }}</span>
+          <van-icon name="arrow-down" class="chevron" />
         </div>
       </div>
-      <div class="form-item">
-        <div class="label">
-          <!-- 请输入钱包地址 -->
-          {{ _t18('withdrawal_Please_enter_address') }}
-        </div>
-        <div class="input">
+
+      <div class="card">
+        <div class="field-label">{{ _t18('withdrawal_Please_enter_address') }}</div>
+        <div class="address-input-wrap">
           <van-field
-            class="code-input address"
+            v-model="form.address"
+            class="address-field"
+            :border="false"
             clearable
             type="text"
             inputmode="text"
@@ -30,86 +26,98 @@
             autocapitalize="off"
             autocorrect="off"
             spellcheck="false"
-            v-model="form.address"
             :placeholder="_t18('withdrawal_Please_enter_address')"
           />
-          <div class="tip" @click="getPaste">
-            <!-- 粘贴 -->
-            {{ _t18('paste') }}
-          </div>
+          <span class="paste" @click="getPaste">{{ _t18('paste') }}</span>
         </div>
-         <!-- 已绑定的钱包地址 --> 
-        <div style="color:#4bbd83" class="label" v-if="address"> 
-          {{ address }}
-        </div> 
-        <div class="label" v-if="email"> 
-          <div class="item">
-            <div class="text">{{ _t18('login_emailCode') }}</div>
-            <div class="input">
-              <input class="code-input" type="text" readonly v-model="form.email" :placeholder="_t18('recharge_input')" />
-            </div>
-          </div>
-        </div>
-        <div class="label" v-if="email"> 
-          <div class="item">
-            <div class="text">{{ _t18('verification_code') }}</div>
-            <div class="input">
-              <van-field type="number" clearable class="code-input code" v-model="form.code" :placeholder="_t18('recharge_input')" />
-              <div  v-if="flag" class="code-box">
-                <div class="wait-code">
-                  <van-count-down :time="time" format="ss" @finish="finish" />
-                </div> 
-              </div>
 
-              <div v-if="!flag&&email" class="code-box"> 
-                 <div class="send-code"  @click="handleSend">{{ _t18('login_send') }}</div>
-              </div>
-              
-            </div>
+        <div v-if="address" class="bound-address">{{ address }}</div>
+
+        <template v-if="email">
+          <div class="field-label sub">{{ _t18('login_emailCode') }}</div>
+          <div class="plain-input-wrap">
+            <input
+              v-model="form.email"
+              class="plain-input"
+              type="text"
+              readonly
+              :placeholder="_t18('recharge_input')"
+            />
           </div>
-        </div>
+          <div class="field-label sub">{{ _t18('verification_code') }}</div>
+          <div class="code-row">
+            <van-field
+              v-model="form.code"
+              type="digit"
+              clearable
+              class="code-field"
+              :border="false"
+              :placeholder="_t18('recharge_input')"
+            />
+            <div v-if="flag" class="code-box">
+              <van-count-down :time="time" format="ss" @finish="finish" />
+            </div>
+            <div v-else class="code-box send" @click="handleSend">{{ _t18('login_send') }}</div>
+          </div>
+        </template>
+
+        <van-button
+          v-if="email"
+          block
+          round
+          class="submit-btn"
+          :loading="loading"
+          @click="bindAddress"
+        >
+          {{ _t18('btnConfirm') }}
+        </van-button>
+        <van-button
+          v-else
+          block
+          round
+          class="submit-btn"
+          :loading="loading"
+          @click="router.push('/email-authentication')"
+        >
+          {{ _t18('sidebar_unbindEmail') }},{{ _t18('sidebar_addEmail') }}
+        </van-button>
+
+        <div v-if="bindSuccessfully" class="bind-success">{{ _t18('Fund_password_set_success') }}</div>
       </div>
-      <van-button v-if="email"  block class="submit" :loading="loading" @click="bindAddress">{{ _t18('btnConfirm') }}</van-button>
-      <van-button v-if="!email" block class="submit" :loading="loading" @click="$router.push('/email-authentication')">{{ _t18('sidebar_unbindEmail') }},{{ _t18('sidebar_addEmail') }}</van-button>
-      <div class="label" style="text-align: center;" v-if="bindSuccessfully">
-          {{ _t18('Fund_password_set_success') }}
-     </div>
     </div>
+
     <van-action-sheet
       v-if="coinList.length"
       v-model:show="coinSheet"
       :actions="coinList"
       @select="onSelect"
       :cancel-text="_t18('cancel')"
+      style="max-width: var(--ex-max-width); left: 50%; translate: -50%"
     />
   </div>
 </template>
 
 <script setup>
-import HeaderBar from '@/components/HeaderBar/index.vue'
+import DarkHeaderBar from '@/components/DarkHeaderBar/index.vue'
 import { storageDict } from '@/config/dict'
 import { updateUserAddressByEmail } from '@/api/account'
-// import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { useUserStore } from '@/store/user/index'
-import { useMainStore } from '@/store/index'
 import { storeToRefs } from 'pinia'
-import { computed, ref, watch, watchEffect } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
+import { showToast } from 'vant'
 import { useToast } from '@/hook/useToast'
 import ERCIMG from '/resource/images/defi/ERC.png'
 import TRCIMG from '/resource/images/defi/TRC.png'
 import { _t18 } from '@/utils/public'
-// import { computed } from 'vue'
-import { _hideAddress } from '@/utils/public'
-import { emailCode, emailBind } from '@/api/user'
+import { emailCode } from '@/api/user'
 
 const { _toast } = useToast()
 const userStore = useUserStore()
-const mainStore = useMainStore()
 const router = useRouter()
 const coinSheet = ref(false)
 const currentIndex = ref(0)
 const iconSrcImg = computed(() => (currentIndex.value == 1 ? TRCIMG : ERCIMG))
-const userId = computed(() => userStore.userInfo.user?.userId)
 const address = computed(() => userStore.userInfo.user?.address)
 const email = computed(() => userStore.userInfo.user?.email)
 const walletType = computed(() => userStore.userInfo.user?.walletType)
@@ -286,9 +294,8 @@ const bindAddress = () => {
       localStorage.setItem(storageDict.USER_INFO, JSON.stringify(userInfo.value))
       // 3) 再异步拉一次服务端，校准最终值（可 await
       await userStore.getUserInfo()
-      bindSuccessfully.value = true;
-      form.value.address = '';
-      address.value = newAddr
+      bindSuccessfully.value = true
+      form.value.address = ''
       // router.back()
     })
     .finally(() => {
@@ -321,162 +328,201 @@ const bindAddress = () => {
 </script>
 
 <style lang="scss" scoped>
-* {
-  color: var(--ex-default-font-color);
-  font-size: 14px;
-}
-:deep(.nav--bar) {
-  background: var(--ex-login-tab-background-color) !important;
-  .nav--bar--text {
-    position: absolute;
-    left: 50px;
-    top: 0;
-  }
-}
-
-.bg {
-  background: var(--ex-login-tab-background-color) !important;
-}
-.form {
+.page-add-address {
   min-height: 100vh;
-
-  background: var(--ex-login-tab-background-color) !important;
-  border-radius: 15px 15px 0px 0px;
-  overflow: hidden;
-  padding: 0 15px 15px;
-  .img {
-    margin-left: 10px;
-    width: 25px;
-    min-width: 25px;
-
-    height: 25px;
-    margin-right: 17px;
-  }
-  .tit {
-    width: 100%;
-    font-size: 16px;
-    font-family: ArialMT;
-    line-height: 18px;
-  }
-  .form-item {
-    .label {
-      font-size: 14px;
-      font-family: PingFangSC-Regular, PingFang SC;
-      font-weight: 400;
-      line-height: 20px;
-      margin: 20px auto 10px;
-      padding-left: 10px;
-    }
-    .input {
-      position: relative;
-      background: var(--ex-login-tab-background-color);
-      border-radius: 4px;
-      height: 40px;
-      display: flex;
-      align-items: center;
-      padding-right: 10px;
-      .coin {
-        width: 25px;
-        height: 25px;
-        margin: 0 10px;
-      }
-      .input-field {
-        width: 100%;
-        line-height: 40px;
-
-        height: 40px;
-        padding: 0;
-        background: transparent;
-        :deep(.van-field__control) {
-          padding-left: 10px;
-        }
-      }
-      .tip {
-        min-width: 40px;
-        text-align: right;
-        font-size: 14px;
-        font-family: PingFangSC-Regular, PingFang SC;
-        font-weight: 400;
-        color: var(--primary-color);
-        line-height: 40px;
-      }
-    }
-  }
-  .submit {
-    margin: 50px auto 30px;
-    background: var(--ex-div-bgColor1);
-    color: var(--ex-font-color);
-    border: none;
-  }
+  background: #05101a;
+  padding-bottom: constant(safe-area-inset-bottom);
+  padding-bottom: env(safe-area-inset-bottom, 0px);
 }
- .send-code{
-      font-size: 14px;
-      border-radius: 5px;
-  }
-  .code-box {
-        min-width: 44px;
-        height: 30px;
-        background: var(--ex-div-bgColor1);
-        border-radius: 2px 2px 2px 2px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: var(--ex-font-color);
-        div {
-          font-size: 14px;
-          color: var(--ex-font-color);
-        }
-      }
- .code-input{
-  width: 100%;
-  height: 36px;
-  padding: 5px;
-  border: 1px solid #dcdfe6;   /* 边框颜色 */
-  border-radius: 6px;          /* 圆角，可按你 UI 调 */
-  outline: none;
+
+.page-body {
+  min-height: calc(100vh - 60px - constant(safe-area-inset-top));
+  min-height: calc(100vh - 60px - env(safe-area-inset-top, 0px));
+  background: #f0f2f5;
+  border-radius: 16px 16px 0 0;
+  padding: 16px 15px 28px;
   box-sizing: border-box;
 }
-.code-input.address{
-   width: 90%;
-   margin-left:10px;
- }
- .code-input.code{
-   width: 40%;
-   margin-right:5px;
- }
-.code-input:focus{
-  border-color: #20b26b;       /* 聚焦时变绿，按你截图的主色 */
-  box-shadow: 0 0 0 2px rgba(32,178,107,.15);
-}
-/* ✅ 这样写才会命中：同一个元素同时拥有 code-input + address */
-.code-input.address:focus{
-  border-color: #20b26b;
-  box-shadow: 0 0 0 2px rgba(32,178,107,.15);
+
+.card {
+  background: #fff;
+  border-radius: 12px;
+  padding: 16px;
+  margin-bottom: 12px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.04);
 }
 
-/* ✅ 同理 */
-.code-input.code:focus{
-  border-color: #20b26b;
-  box-shadow: 0 0 0 2px rgba(32,178,107,.15);
-}
-.el-input__inner:focus{
-  border-color:#20b26b;
-  box-shadow:0 0 0 2px rgba(32,178,107,.15);
+.field-label {
+  font-size: 13px;
+  color: #969799;
+  margin-bottom: 10px;
+  line-height: 1.4;
+
+  &.sub {
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
 }
 
-/* van-field 内部真实 input */
-.code-input .van-field__control{
-  border: 1px solid #dcdfe6;
-  border-radius: 6px;
+.chain-select {
+  display: flex;
+  align-items: center;
+  min-height: 48px;
   padding: 0 12px;
-  height: 36px;
-  box-sizing: border-box;
+  background: #f7f8fa;
+  border-radius: 8px;
+  border: 1px solid #ebedf0;
+  cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+
+  .coin {
+    width: 28px;
+    height: 28px;
+    margin-right: 12px;
+    flex-shrink: 0;
+  }
+
+  .chain-name {
+    flex: 1;
+    font-size: 16px;
+    font-weight: 500;
+    color: #323233;
+  }
+
+  .chevron {
+    color: #c8c9cc;
+    font-size: 16px;
+    flex-shrink: 0;
+  }
 }
 
-/* focus 命中内部 input */
-.code-input .van-field__control:focus{
-  border-color: #20b26b;
-  box-shadow: 0 0 0 2px rgba(32,178,107,.15);
+.address-input-wrap {
+  display: flex;
+  align-items: center;
+  border: 1px solid #ebedf0;
+  border-radius: 8px;
+  padding: 0 4px 0 12px;
+  background: #fff;
+  min-height: 48px;
+}
+
+.address-field {
+  flex: 1;
+  padding: 0;
+  background: transparent;
+}
+
+.page-add-address :deep(.address-field .van-field__body) {
+  padding: 8px 0;
+}
+
+.page-add-address :deep(.address-field .van-field__control) {
+  font-size: 15px;
+  color: #323233;
+
+  &::placeholder {
+    color: #c8c9cc;
+  }
+}
+
+.paste {
+  flex-shrink: 0;
+  padding: 8px 12px 8px 8px;
+  font-size: 14px;
+  color: #17ac74;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.bound-address {
+  margin-top: 12px;
+  font-size: 13px;
+  line-height: 1.5;
+  word-break: break-all;
+  color: #17ac74;
+}
+
+.plain-input-wrap {
+  border: 1px solid #ebedf0;
+  border-radius: 8px;
+  padding: 0 12px;
+  background: #f7f8fa;
+}
+
+.plain-input {
+  width: 100%;
+  height: 44px;
+  border: none;
+  background: transparent;
+  font-size: 14px;
+  color: #323233;
   outline: none;
+
+  &::placeholder {
+    color: #c8c9cc;
+  }
+}
+
+.code-row {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  border: 1px solid #ebedf0;
+  border-radius: 8px;
+  padding: 0 8px 0 12px;
+  min-height: 48px;
+}
+
+.code-field {
+  flex: 1;
+  padding: 0;
+  background: transparent;
+}
+
+.page-add-address :deep(.code-field .van-field__body) {
+  padding: 8px 0;
+}
+
+.page-add-address :deep(.code-field .van-field__control) {
+  font-size: 15px;
+  color: #323233;
+}
+
+.code-box {
+  flex-shrink: 0;
+  min-width: 52px;
+  height: 32px;
+  padding: 0 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  color: #17ac74;
+
+  &.send {
+    cursor: pointer;
+    -webkit-tap-highlight-color: transparent;
+  }
+}
+
+.page-add-address :deep(.code-box .van-count-down) {
+  font-size: 14px;
+  color: #17ac74;
+}
+
+.submit-btn {
+  margin-top: 28px;
+  height: 48px;
+  font-size: 15px;
+  font-weight: 500;
+  border: none;
+  background: #00a619 !important;
+  color: #fff !important;
+}
+
+.bind-success {
+  margin-top: 16px;
+  text-align: center;
+  font-size: 14px;
+  color: #17ac74;
 }
 </style>
