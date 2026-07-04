0b35d4dc71ddf62075c05af8560d6d644948418a
minxi200 <2>
2026-07-04 20:09:05 +0800

    取消强制置顶

--- vam-h5/src/views/assets/myassetsHome.vue
+++ vam-h5/src/views/assets/myassetsHome.vue
@@ -152,8 +152,7 @@
       obj.title = isUsdt ? 'USDT' : sym.replace(/usdt/ig, '').trim().toUpperCase()
       obj.icon = isUsdt ? 'usdt' : sym.replace(/usdt/ig, '').trim()
   
-      if (isUsdt) list.unshift(obj)
-      else list.push(obj)
+      list.push(obj)
     })
     return list
   })
